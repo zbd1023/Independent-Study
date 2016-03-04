06f93c5e487f85d0138fdca5cc5918bd7e4b8477
Daniel Chai <Daniel@wl-10-190-192-250.wireless.duke.edu>
2016-03-04 00:15:38 -0500

    Made code cleaner

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,6 @@
 var Firebase = require('firebase');
 'use strict';
+
 import React, {
   AppRegistry,
   Component,
@@ -38,25 +39,24 @@ var Main = React.createClass({
 
   render() {
     return (
-      <TabBarIOS
-      tintColor="black"
-      barTintColor="silver">
-
-      <TabBarIOS.Item
-      title="Settings"
-      icon={require('./settings.png')}
-      //icon={{uri: base64Icon, scale: 3}}
-      selected={this.state.selectedTab === 'blueTab'}
-      onPress={() => {
+      <TabBarIOS tintColor="black" barTintColor="silver">
+        <TabBarIOS.Item
+          title="Settings"
+          icon={require('./settings.png')}
+          //icon={{uri: base64Icon, scale: 3}}
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
             this.setState({
               selectedTab: 'blueTab',
             });
-            }}>
-        <View>
-        <List    />
-				</View>
-      </TabBarIOS.Item>
-      <TabBarIOS.Item
+          }}
+        >
+          <View>
+            <List style = {styles.list} />
+				  </View>
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
           systemIcon="history"
           badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
           selected={this.state.selectedTab === 'redTab'}
@@ -65,16 +65,15 @@ var Main = React.createClass({
               selectedTab: 'redTab',
               notifCount: this.state.notifCount + 1,
             });
-          }}>
-		<View style = {styles.second}>
-			  <Text>
-		asldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafh
-		</Text>
-		</View>
-		</TabBarIOS.Item>
-
+          }}
+        >
+		      <View style = {styles.sampleText}>
+			      <Text>
+		          asldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafhsfhasdasldkfjsdkajakshsdafh
+            </Text>
+		      </View>
+		    </TabBarIOS.Item>
       </TabBarIOS>
-
     );
   }
 });
@@ -82,82 +81,29 @@ var Main = React.createClass({
 class myApp extends React.Component {
   render() {
     return (
-
       <NavigatorIOS
         style={styles.nav}
         initialRoute= {{
           title:'Some App Name',
           component: Main,
-          //leftButtonIcon:
-          //leftButtonIcon: {uri: base64Icon, scale: 3},
-          //leftButtonTitle:"lll"
-          //icon={require('./flux.png')}
         }}
-
       />
-
-      // <View style={styles.container}>
-      //   <View style={styles.top}>
-      //     <Text style={styles.appName}>
-      //       Some App Name
-      //     </Text>
-      //   </View>
-      // </View>
-
+      //comment
     );
+  }
 }
-}
+
 var styles = StyleSheet.create({
-	second:{
+  list: {
+    paddingTop: 65,
+  },
+	sampleText: {
 		paddingTop: 65,
 	},
-  nav:{
+  nav: {
     flex:1,
     backgroundColor: '#111111',
   },
-  top:{
-    marginTop: 20,
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-
-  },
-  appName:{
-    color: '#FFFFFF',
-    textAlign: 'center',
-  },
-
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: '#FFFFFF',
-  },
-  leftContainer:{
-    marginLeft:10,
-  },
-  thumbnail:{
-    width: 53,
-    height: 81,
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title:{
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-    color: '#FFFFFF',
-
-  },
-  year:{
-    textAlign: 'center',
-    color: '#AFFFFF',
-
-  },
-
-
 });
 
 AppRegistry.registerComponent('myApp', () => myApp);
