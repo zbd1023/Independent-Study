851881e874204356cf48898d7978660ca0d32bdc
Kiyoon Ko <kiyoonko@gmail.com>
2016-03-19 20:17:05 -0400

    Tried Firebase integration

--- listView.js
+++ listView.js
@@ -9,36 +9,69 @@ import React, {
   View,
   ListView,
 } from 'react-native';
+const Firebase = require('firebase');
+const FirebaseUrl = 'https://devils-reminder.firebaseio.com/';
 var ListItem = require('./ListItem'); // Require list item class from ListItem.js
 
 var Main = React.createClass({
+
   statics: {
     title: '<ListView>',
     description: 'Performant, scrollable list of data.'
   },
 
-
-  _renderItem(item) {
-    return (
-      <ListItem item={item} onPress={() => {}} />
-      /* how to display a list item */
-    );
-  },
-
-  getInitialState: function() {
+  getInitialState () {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     return {
-      dataSource: ds.cloneWithRows(['Class 1', 'Class 2']),
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
     };
+    this.itemsRef = this.getRef().child('Classes');
   },
 
-  render: function() {
+  getRef() {
+    return new Firebase(FirebaseUrl);
+  },
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', snap => {
+
+      // get children as an array
+      var items = [];
+      snapshot.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key()
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  },
+
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  },
+
+  render(){
     return (
       <ListView
         dataSource = {this.state.dataSource}
         renderRow={this._renderItem}
         style={styles.listview}/>
       /*render a list view */
+    );
+  },
+
+  _renderItem(item) {
+    return (
+      <ListItem item={item} onPress={() => {}} />
+      /* how to display a list item */
     );
   },
 });
