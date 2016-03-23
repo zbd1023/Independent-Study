9d62a56918b4f1ffdb6af4893a858a5b4549fdd4
Daniel Chai <Daniel@wl-10-191-4-229.wireless.duke.edu>
2016-03-22 23:16:23 -0400

    Remade app

--- ListItem.js
+++ ListItem.js
@@ -1,34 +1,21 @@
 'use strict';
-var React = require('react-native');
-const { View, TouchableHighlight, Text ,StyleSheet} = React;
+const React = require('react-native');
+const {View, TouchableHighlight, Text} = React;
+
+const styles = require('./styles.js');
 
 class ListItem extends React.Component {
   render() {
     return (
-      <TouchableHighlight onPress={this.props.onPress}>
-        <View style={styles.li}>
-          <Text style={styles.liText}>{this.props.item}</Text>
-          <Text>this.props.item is the dataSource</Text>
+      <TouchableHighlight onPress = {this.props.onPress}>
+        <View style = {styles.li}>
+          <Text style = {styles.liText}>
+            {this.props.item.title}
+          </Text>
         </View>
       </TouchableHighlight>
     );
   }
 }
-
-var styles = StyleSheet.create({
-  li: {
-  backgroundColor: '#fff',
-  borderBottomColor: '#eee',
-  borderColor: 'transparent',
-  borderWidth: 1,
-  paddingLeft: 16,
-  paddingTop: 14,
-  paddingBottom: 16,
-},
-liText: {
-    color: '#333',
-    fontSize: 16,
-  },
-});
 
 module.exports = ListItem;
---
+++ StatusBar.js
@@ -0,0 +1,22 @@
+'use strict';
+const React = require('react-native');
+const {StyleSheet, Text, View} = React;
+
+const styles = require('./styles.js');
+
+class StatusBar extends React.Component {
+  render() {
+    return (
+      <View>
+        <View style = {styles.statusbar} />
+        <View style = {styles.navbar}>
+          <Text style = {styles.navbarTitle}>
+            {this.props.title}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+module.exports = StatusBar;
